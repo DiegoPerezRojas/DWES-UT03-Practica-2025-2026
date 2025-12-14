cf76d627f57691df90aa4d92c98646a66a400031
DiegoPerezRojas <84037693+DiegoPerezRojas@users.noreply.github.com>
2025-12-14 17:46:39 +0100

    Tarea 3

---
+++ tareas_dwes/tareas/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+from django.db import models
+
+class Tarea(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    completada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_recordatorio = models.DateTimeField()
+
+    def __str__(self):
+        return self.titulo
+
